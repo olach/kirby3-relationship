8071ba3e25e22d21db2be7d84610accd9339e3cb
Ola Christensson <ola.christensson@grandpublic.se>
2018-10-29 23:39:56 +0100

    wip

--- index.js
+++ index.js
@@ -19,36 +19,94 @@ var RelationshipSearch = {
 }
 
 var RelationshipListbox = {
-	inheritAttrs: true,
+	inheritAttrs: false,
 	props: {
 		items: Array,
+		selected: {
+			type: Array,
+			default: function () {
+				return [];
+			}
+		},
 		sortable: Boolean,
 		multiselectable: Boolean,
 		deletable: Boolean
 	},
 	data: function () {
 		return {
-			items: this.items
+			items: this.items,
+			selected: this.selected
 		};
 	},
 	watch: {
 		items: function () {
-			console.log(this.items);
+			//console.log(this.items);
 			//this.$emit('input', this.items);
 		}
 	},
 	methods: {
 		onInput() {
 			//this.$emit("input", this.selected);
+		},
+		index: function (option) {
+			return this.state.findIndex(item => item.value === option.value);
+		},
+		isSelected: function (item) {
+			// todo: Use findIndex instead:
+			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+			for (var i = 0; i < this.selected.length; i++) {
+				if (this.selected[i].value === item.value) {
+					return true;
+				}
+			}
+			
+			return false;
+		},
+		selectItem: function (item) {
+			this.selected.push(item);
+		},
+		unselectItem: function (item) {
+			// todo: Use findIndex instead:
+			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+			for (var i = 0; i < this.selected.length; i++) {
+				if (this.selected[i].value === item.value) {
+					this.selected.splice(i, 1);
+					return;
+				}
+			}
+			
+//			var index = this.selected.indexOf(item);
+			
+//			if (index !== -1) {
+//				this.selected.splice(index, 1);
+//			}
+		},
+		removeItem: function (item) {
+			if (this.deletable === false) {
+				return false;
+			}
+			
+			var index = this.items.indexOf(item);
+			
+			if (index !== -1) {
+				this.items.splice(index, 1);
+			}
+		},
+		toggleItem: function (item) {
+			if (this.isSelected(item)) {
+				this.unselectItem(item);
+			} else {
+				this.selectItem(item);
+			}
 		}
 	},
 	template: `
-		<k-draggable element="ul" :list="items" :options="{'disabled':!sortable}" role="listbox" tabindex="0">
-			<li v-for="item in items">
-				<span v-if="sortable" class="relationship-item-sort"><k-icon :type="'sort'" /></span>
+		<k-draggable element="ul" :list="items" :options="{'disabled':!sortable,animation:150}" role="listbox" :data-sortable="sortable" tabindex="0">
+			<li v-for="item in items" :aria-selected="isSelected(item)" role="option" @click="toggleItem(item)">
+				<k-icon v-if="sortable" class="relationship-item-sort" :type="'sort'" />
 				<span class="relationship-item-label">{{item.text}}</span>				
 				<k-button v-if="multiselectable" tabindex="-1" icon="add"></k-button>
-				<k-button v-if="deletable" tabindex="-1" icon="remove"></k-button>
+				<k-button v-if="deletable" @click="removeItem(item)" tabindex="-1" icon="remove"></k-button>
 			</li>
 		</k-draggable>
 	`
@@ -62,9 +120,9 @@ panel.plugin('olach/relationship', {
 				help: String,
 				label: String,
 				name: String,
+				value: Array,
 				options: Array,
 				search: Boolean,
-				value: Array,
 			},
 			components: {
 				'relationship-search': RelationshipSearch,
@@ -72,7 +130,7 @@ panel.plugin('olach/relationship', {
 			},
 			methods: {
 				addItem: function (item) {
-					console.log(this.$refs);
+					//console.log(this.$refs);
 					this.value.push(item);
 				},
 				removeItem: function (item) {
@@ -98,11 +156,20 @@ panel.plugin('olach/relationship', {
 					return this.value.findIndex(item => item.value === theitem.value);
 				},
 				onInput: function () {
+					console.log('onInput');
 					this.$emit("input", this.selected);
+				}
+			},
+			watch: {
+				items: function () {
+					//console.log(this.items);
+					this.$emit('input', this.items);
 				}
 			},
 			computed: {
 				selected: function () {
+					//console.log(this.value[0]);
+					//console.log(options[0]);
 					return this.value;
 				}
 			},
@@ -121,7 +188,7 @@ panel.plugin('olach/relationship', {
 					</div>
 					
 					<div class="relationship-lists">							
-						<relationship-listbox class="relationship-list relationship-list--available" :items="available" :sortable="false" :multiselectable="true" :deletable="false" />
+						<relationship-listbox class="relationship-list relationship-list--available" :items="available" :selected="selected" :sortable="false" :multiselectable="true" :deletable="false" />
 						<relationship-listbox class="relationship-list relationship-list--selected" :items="selected" :sortable="true" :multiselectable="false" :deletable="true" />
 						
 						<!--
