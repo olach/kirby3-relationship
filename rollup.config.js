8ee77cbba910724d157f62f9aa9f1639720b12a7
Ola Christensson <ola.christensson@grandpublic.se>
2018-12-09 01:05:28 +0100

    New build setup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel';
+import minify from 'rollup-plugin-babel-minify';
+import postcss from 'rollup-plugin-postcss';
+import vue from 'rollup-plugin-vue';
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+export default {
+	input: 'src/main.js',
+	output: {
+		file: 'index.js',
+		format: 'iife'
+	},
+	plugins: [
+		vue({
+			css: false,
+		}),
+		babel(),
+		minify({
+			comments: !PRODUCTION,
+			sourceMap: !PRODUCTION
+		}),
+		postcss({
+			extract: true,
+			minimize: PRODUCTION,
+			sourceMap: PRODUCTION ? false : 'inline'
+		})
+	  ]
+};
