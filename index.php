d400ce2826e8371ce9070d34e1a84df38a6d5433
Ola Christensson <ola.christensson@grandpublic.se>
2019-03-16 22:25:46 +0100

    Documentation

--- index.php
+++ index.php
@@ -5,8 +5,16 @@ Kirby::plugin('olach/relationship', [
 		'relationship' => [
 			'extends' => 'tags',
 			'props' => [
+				
+				/**
+				 * Unset inherited props
+				 */
 				'accept' => null,
 				'icon' => null,
+				
+				/**
+				 * Enable/disable the search in the dropdown
+				 */
 				'search' => function (bool $search = false) {
 					return $search;
 				}
