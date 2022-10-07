6ebdf1bc33fec3e23cb6700cb8500bcbed67d8fd
Ola Christensson <ola.christensson@grandpublic.se>
2022-10-07 16:10:49 +0200

    Fix compatibility with Kirby 3.8

--- index.php
+++ index.php
@@ -18,7 +18,49 @@ Kirby::plugin('olach/relationship', [
 				'search' => function (bool $search = false) {
 					return $search;
 				}
-			]
+			],
+			'computed' => [
+				'default' => function (): array {
+					return $this->toTags($this->default);
+				},
+				'value' => function (): array {
+					return $this->toTags($this->value);
+				}
+			],
+			'methods' => [
+				'toTags' => function ($value) {
+					if (is_null($value) === true) {
+						return [];
+					}
+
+					$options = $this->options();
+
+					// transform into value-text objects
+					return array_map(function ($option) use ($options) {
+						// already a valid object
+						if (is_array($option) === true && isset($option['value'], $option['text']) === true) {
+							return $option;
+						}
+
+						$index = array_search($option, array_column($options, 'value'));
+
+						if ($index !== false) {
+							return $options[$index];
+						}
+
+						return [
+							'value' => $option,
+							'text'  => $option,
+						];
+					}, Str::split($value, $this->separator()));
+				}
+			],
+			'save' => function (array $value = null): string {
+				return A::join(
+					A::pluck($value, 'value'),
+					$this->separator() . ' '
+				);
+			},
 		]
 	]
 ]);
